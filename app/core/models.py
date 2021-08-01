bf916ff108cab05579fa3735d922497709ee013e
malikgr8 <ahmad.cuteboy98@gmail.com>
2021-08-02 01:36:54 +0500

    adding user custom model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.base_user import BaseUserManager
+# from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
@@ -8,9 +8,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # Creates and save a new user
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('user must have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        # Creating and saving superuser
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
         return user
 
 
@@ -22,5 +33,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
